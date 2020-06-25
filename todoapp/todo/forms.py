8d5c688615881b485d5c68bf724c57bd71ade9f8
pavan <mandadipavan.mp@gmail.com>
2020-06-25 17:37:05 +0530

    updated

--- todoapp/todo/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import ModelData
-
-class TodoForm(forms.ModelForm):
-
-	class Meta:
-		model = ModelData
-		fields = ['title']
--- todoapp/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns=[
-path('', views.TodoView, name='TodoView'),
-path('update_list/<int:pk>/', views.UpdateList, name='update_list'),
-path('delete_list/<int:pk>/', views.DeleteList, name='delete_list'),
-]
